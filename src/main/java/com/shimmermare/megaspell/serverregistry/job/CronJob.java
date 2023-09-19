d4762dd19f891c3fa7d9ed179bdf98d996cdada8
Shimmermare <shimmermare@gmail.com>
2023-09-19 22:48:39 +0300

    Fix job bean init order

--- src/main/java/com/shimmermare/megaspell/serverregistry/job/CronJob.java
+++ src/main/java/com/shimmermare/megaspell/serverregistry/job/CronJob.java
@@ -2,6 +2,7 @@ package com.shimmermare.megaspell.serverregistry.job;
 
 import org.quartz.Scheduler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
@@ -13,6 +14,7 @@ import java.lang.annotation.Target;
  * Classes implementing {@link org.quartz.Job} that are marked with this annotation will be
  * automatically scheduled for execution.
  */
+@DependsOn("jobScheduler")
 @ConditionalOnBean(Scheduler.class)
 @Component
 @Retention(RetentionPolicy.RUNTIME)
